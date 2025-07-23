0485b7ae01cac7b191183b26ffc84a31530e1ceb
Namie-Adanna <namiekoks@gmail.com>
2025-07-23 12:27:26 +0300

    modified client

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Sparkles, Phone, Mail, MapPin, Facebook, Twitter, Instagram } from 'lucide-react';
 
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createContext, useContext, useReducer, useEffect } from 'react';
 import { authAPI } from '../services/api';
 import toast from 'react-hot-toast';
